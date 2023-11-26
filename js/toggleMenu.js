78c16f31616d771003c452a04335cd4c5e856363
Nahuel Gonzalo Farut <farutnen@gmail.com>
2023-11-26 16:47:41 -0300

    add casino

---
+++ js/toggleMenu.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const nav = document.querySelector('.nav');
+    toggleMenu.classList.toggle('active')
+    nav.classList.toggle('active')
+}
